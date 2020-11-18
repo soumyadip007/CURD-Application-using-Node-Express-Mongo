c2b7a6af9bafd4c48233c5e9aa56ccd71e726a22
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-11-18 22:39:54 +0530

    App.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app=express()
+
+const url='mongodb://localhost:27017/mi'
+
+mongoose.connect(url, {useNewUrlParser:true})
+const con=mongoose.connection
+
+con.on('open', function(){
+    console.log('connected')
+})
