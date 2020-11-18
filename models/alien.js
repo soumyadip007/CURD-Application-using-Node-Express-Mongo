3f408638b8bfb8a77100f8e9bf2876fa0a7f9fb7
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-11-18 23:03:20 +0530

    aliens.js DAO

---
+++ models/alien.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+
+
+const alienSchema= new mongoose.Schema({
+
+    name:{
+        type:String,
+        required: true
+    },
+    tech:{
+        type:String,
+        required: true
+    },
+    sub:{
+        type:String,
+        required: true,
+        default:false
+    }
+})
